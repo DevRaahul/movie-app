b769545b7ff412089b261cb5c0c6df1850fcfec4
Rahul <mail.rahulbhoir@gmail.com>
2024-12-20 02:00:48 +0530

    added dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import ErrorBoundary from "./ErrorBoundary/ErrorBoundary.tsx";
 import "./index.css";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary fallback={<p>Somthing went wrong...</p>}>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </ErrorBoundary>
 );
