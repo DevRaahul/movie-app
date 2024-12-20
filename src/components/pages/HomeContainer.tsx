8570fb66ef5abf84beb08e52c7ce1c8e1af7c632
Rahul <DevRaahul>
2024-12-20 20:19:01 +0530

    added loader component and tab icon

--- src/components/pages/HomeContainer.tsx
+++ src/components/pages/HomeContainer.tsx
@@ -1,5 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "../ui/button";
+
 const HomeContainer = () => {
-  return <div>HomeContainer</div>;
+  const { logout } = useAuth0();
+  return (
+    <div>
+      <Button onClick={() => logout()}>Home Container</Button>
+    </div>
+  );
 };
 
 export default HomeContainer;
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,21 +1,46 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
-import LoginForm from "./LoginForm";
+import { KeySquare } from "lucide-react";
+import { Button } from "../ui/button";
+import { LoaderComponent } from "../common/LoaderComponent";
 
 const LoginPage: FC = () => {
+  const { isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    debugger;
+    if (isAuthenticated) {
+      navigate("/home");
+    }
+  }, [isAuthenticated]);
+
+  const handleLoginWithAuth = () => {
+    loginWithRedirect();
+  };
+
   return (
     <>
       <div className="h-[90vh]">
         <div className="h-full flex justify-center items-center flex-col">
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-3xl font-normal">Login</CardTitle>
-              <CardDescription>Enter your email below to login into your account.</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <LoginForm />
-            </CardContent>
-          </Card>
+          {isLoading ? (
+            <LoaderComponent size="large" show />
+          ) : (
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-3xl font-normal">Login</CardTitle>
+                <CardDescription>Enter your email below to login into your account.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Button type="button" className="w-full" onClick={handleLoginWithAuth}>
+                  <KeySquare />
+                  Continue with Google
+                </Button>
+              </CardContent>
+            </Card>
+          )}
         </div>
       </div>
     </>
