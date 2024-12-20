6fded3df0174298ee4f528761e69ddddf0efcd96
Rahul <mail.rahulbhoir@gmail.com>
2024-12-20 16:13:35 +0530

    added global config data call

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,23 @@
 import App from "@/App";
+import axiosInstance from "@/constant/AxiosConfig";
+import { useGlobalContext } from "@/context/GlobalContext";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const AppRoutes = () => {
+  const [, updateGlobalData] = useGlobalContext();
+
+  const fetchConfigData = () => {
+    axiosInstance.get("configuration").then((res) => {
+      const imgData = res.data?.images;
+      const { base_url: img_base_url } = imgData;
+      updateGlobalData({ imgUrl: img_base_url });
+    });
+  };
+
+  useEffect(() => {
+    fetchConfigData();
+  }, []);
   return (
     <>
       <BrowserRouter>
